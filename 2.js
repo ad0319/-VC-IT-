cd935839cea2e9ef18a0bfedc09ce181e829a880
ad0319 <103395227+ad0319@users.noreply.github.com>
2022-04-27 13:33:28 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,19 +1,18 @@
-
 function validateform(){ 
     var name=document.myform.name.value; 
     var aname=document.myform.aname.value;
     var bname=document.myform.bname.value; 
     var number=document.myform.number.value; 
      
-    if (name==null || name==""){ //if statement to check if the input box is empty
+    if (name==null || name==""){ //If statement checks whether the input box has content
      alert("Name can't be blank"); 
      return false; 
     } 
-    else if(aname==null || aname==""){ //if statement to check if the input box is empty
+    else if(aname==null || aname==""){ //If statement checks whether the input box has content
      alert("Name can't be blank2"); 
      return false; 
     } 
-    else if(bname==null || bname==""){ //if statement to check if the input box is empty
+    else if(bname==null || bname==""){ //If statement checks whether the input box has content
      alert("Name can't be blank3"); 
     return false; 
     } 
